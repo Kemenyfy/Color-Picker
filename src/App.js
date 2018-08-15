779831b29fbba2e7245c7a3ba7a62f03629c188c
Kemenyfy <gustavo.kemenyfy@gmail.com>
2018-08-15 16:00:56 -0400

    Basic Slider is Working with CSS

--- src/App.js
+++ src/App.js
@@ -1,18 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+        hue: "180", 
+        saturation: "50",
+        lightness: "50"
+    }
+  }
+
+  updateColor = (event) => {
+    const newColor = event.target.value;
+    console.log('updating Color', newColor)
+  }
+
+  hueUpdated = (event) => {
+    const newHue = event.target.value;
+    this.setState({hue: newHue})
+  }
+
+  saturationUpdated = (event) => {
+    const newSaturation = event.target.value;
+    this.setState({saturation: newSaturation})
+  }
+
+  lightnessUpdated = (event) => {
+    const newLightness = event.target.value;
+    this.setState({lightness: newLightness})
+  }
+
+  // alphaUpdated = (event) => {
+  //   const newAlpha = event.target.value / 100
+  //   this.setState({alpha: newAlpha})
+  // }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header className="App-Header">
+          <h5 className="App-Title-h5">The Best</h5>
+          <h2 className="App-Title-h2">Color Picker</h2>
+          <h6 className="App-Title-h6">Created by Someone/Somehow/Maybe</h6>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <section className="Main-Picker">
+          <div style= {
+            {
+            height: '80px',
+            width: '80px',
+            backgroundColor: `hsl(${this.state.hue},${this.state.saturation}%,${this.state.lightness}%)`}}>
+          </div>
+          <section className="Sliders">
+            <input type="range" min="0" max="360" onInput={this.updateColor} onChange={this.hueUpdated} value={this.state.hue}/>
+            <input type="range" min="0" max="100" onInput={this.updateColor} onChange={this.saturationUpdated} value={this.state.saturation}/>
+            <input type="range" min="0" max="100" onInput={this.updateColor} onChange={this.lightnessUpdated} value={this.state.lightness}/>
+            {/* <input type="range" min="0" max="100" onInput={this.updateColor} onChange={this.alphaUpdated} value={this.state.alpha}/> */}
+          </section>  
+        </section>
       </div>
     );
   }

0c83db621ae3864e1b3f92dcd07e565ee0dd83d0
Kemenyfy <gustavo.kemenyfy@gmail.com>
2018-08-16 14:07:02 -0400

    Added Color Hex Box at Bottom, refactored a bit

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        hue: "180", 
-        saturation: "50",
-        lightness: "50"
+      hue: "180",
+      saturation: "50",
+      lightness: "50"
     }
   }
 
@@ -19,17 +19,17 @@ class App extends Component {
 
   hueUpdated = (event) => {
     const newHue = event.target.value;
-    this.setState({hue: newHue})
+    this.setState({ hue: newHue })
   }
 
   saturationUpdated = (event) => {
     const newSaturation = event.target.value;
-    this.setState({saturation: newSaturation})
+    this.setState({ saturation: newSaturation })
   }
 
   lightnessUpdated = (event) => {
     const newLightness = event.target.value;
-    this.setState({lightness: newLightness})
+    this.setState({ lightness: newLightness })
   }
 
   // alphaUpdated = (event) => {
@@ -41,23 +41,33 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-Header">
-          <span className="App-Title-1">The Best</span>
-          <span className="App-Title-2">Color Picker</span>
-          <span className="App-Title-3">Created by Gustavo Kemenyfy</span>
-        </header>
-        <section className="Main-Picker">
-          <div className="ColorBox" style= {
+          <span className="App-Title-1">A Pretty Useful</span>
+          <span className="App-Title-2" style={
             {
-            backgroundColor: `hsl(${this.state.hue},${this.state.saturation}%,${this.state.lightness}%)`}}>
-          </div>
-          <section className="Sliders">
-            <caption className="Captions">Hue</caption>
-            <input type="range" min="0" max="360" onInput={this.updateColor} onChange={this.hueUpdated} value={this.state.hue}/>
-            <caption className="Captions">Saturation</caption>
-            <input type="range" min="0" max="100" onInput={this.updateColor} onChange={this.saturationUpdated} value={this.state.saturation}/>
-            <caption className="Captions">Lightness</caption>
-            <input type="range" min="0" max="100" onInput={this.updateColor} onChange={this.lightnessUpdated} value={this.state.lightness}/>
-            {/* <input type="range" min="0" max="100" onInput={this.updateColor} onChange={this.alphaUpdated} value={this.state.alpha}/> */}
+              color: `hsl(${this.state.hue},${this.state.saturation}%,${this.state.lightness}%)`
+            }}>Color Picker</span>
+          <span className="App-Title-3">Created by Somebody/Somehow/Maybe</span>
+        </header>
+        <section>
+          <section className="Main-Picker">
+            <div className="ColorBox" style={
+              {
+                backgroundColor: `hsl(${this.state.hue},${this.state.saturation}%,${this.state.lightness}%)`
+              }}>
+            </div>
+            <section className="Sliders">
+              <div className="Captions">Hue</div>
+              <input type="range" min="0" max="360" onInput={this.updateColor} onChange={this.hueUpdated} value={this.state.hue} />
+              <div className="Captions">Saturation</div>
+              <input type="range" min="0" max="100" onInput={this.updateColor} onChange={this.saturationUpdated} value={this.state.saturation} />
+              <div className="Captions">Lightness</div>
+              <input type="range" min="0" max="100" onInput={this.updateColor} onChange={this.lightnessUpdated} value={this.state.lightness} />
+              {/* <caption className="Captions">Alpha</caption>
+            <input type="range" min="0" max="1" onInput={this.updateColor} onChange={this.alphaUpdated} value={this.state.alpha}/> */}
+            </section>
+          </section>
+          <section>
+            <input className="ColorInputs" placeholder={`hsl(${this.state.hue},${this.state.saturation}%,${this.state.lightness}%)`} type="text"/>
           </section>  
         </section>
       </div>
